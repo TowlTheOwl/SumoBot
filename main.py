6c9740dae32129fd0ceadde593d1c3aecf99114f
Henry Yang <88568778+ShroomOfDoomGoVroom@users.noreply.github.com>
2021-08-07 13:33:55 -0700

    Update main.py
    WORKING! LES GO

--- main.py
+++ main.py
@@ -17,155 +17,134 @@ def compareTuples(list_a, list_b, threshold):
     return passed
 
 
-
-import concurrent.futures
+import numpy as np
 import time
 from ctypes import windll
 from os import path, remove
 from random import randint
-
 import keyboard
 import win32api
 import win32con
-from PIL import ImageGrab, Image
+import mss
+import mss.tools
+from multiprocessing import Process, Queue
 
 
 # CAPTURE THE SCREEN (TAKE A SCREENSHOT)
-def screen_capture():
-    img = ImageGrab.grab(bbox=(0, 0, 1920, 1080), include_layered_windows=True)
-    print("Captured!")
-    img.save(r'img.jpg')
+def screen_grab(queue1):
+    while force_stop() != 2:
+        with mss.mss() as sct:
+            mon = sct.monitors[1]
+            queue1.put(sct.grab(mon))
 
 
 # USE MACHINE LEARNING TO CLASSIFY THE ACTION
-def classify():
-    print('classify')
-    while not path.isfile(r'img.jpg'):
-        continue
-    try:
-        image = Image.open(open(r"img.jpg", 'rb'))
-        pixelMap = image.load()
-    except Exception as ec:
-        print(ec)
-        image = None
-        #print(image)
+def classify(queue1, queue2):
+    while force_stop() != 2:
+        if force_stop() == 1:
+            image = queue1.get()
+            if image is not None:
+                img_array = np.array(image)
+                # DOES SOME CALCULATION
 
-
-    if image is not None:
-        last_pixel = pixelMap[1919, 1079]
-        if last_pixel == (128, 128, 128):
-            #print('image not completed')
-            pass
-        else:
-
-            # DOES SOME CALCULATION
-
-            # OUTPUTS 14-DIGIT OUTPUT
-            for i in range(8):
-                value = randint(0, 1)
-                if i == 0:
-                    output = str(value)
-                else:
+                # OUTPUTS 14-DIGIT OUTPUT
+                for i in range(8):
+                    value = randint(0, 1)
+                    if i == 0:
+                        output = str(value)
+                    else:
+                        output = output + str(value)
+                for i in range(6):
+                    value = randint(0, 9)
                     output = output + str(value)
-            for i in range(6):
-                value = randint(0, 9)
-                output = output + str(value)
-            with open('action.txt', 'w') as data:
-                print(output, file=data)
-    else:
-        #print('bad image')
-        pass
+                queue2.put(output)
+            else:
+                pass
 
-    #print('task finished')
 
 # USE THE OUTPUT TO MAKE ACTION
-def mojimo(acc='ctrl+g'):
-    print('mojimo')
-    with open('action.txt', 'r') as data:
-        action = data.read()
-        print(1)
-    if action != '':
-        st = time.time()
-        print(2)
-        # setup the local vars.
-        print(f'current action: {action}')
+def execute_action(queue2, acc='ctrl+g'):
+    while force_stop() != 2:
+        if force_stop() == 1:
+            action = queue2.get()
+            if action is not None:
+                st = time.time()
+                # setup the local vars.
+                print(f'current action: {action}')
 
-        duration = 0.05  # actions taken every 'duration' in sec.
-        # to be set to 0 because the same action is to be taken until the next line updates.
-        t = time.time()
-        keylist = []
-        autoclicker = False
-        autoclickerstatus = False  # saves past status of autoclicker
-        mouse = tuple()
-        # ----
-        line = list(map(int, action))
-        if len(line) < 13:
-            raise AssertionError
-        if line[0] == 1:
-            keylist.append('w')
-        if line[1] == 1:
-            keylist.append('a')
-        if line[2] == 1:
-            keylist.append('s')
-        if line[3] == 1:
-            keylist.append('d')
-        if line[4] == 1:
-            keylist.append('shift')
-        if line[5] == 1:
-            keylist.append('ctrl')
-        if line[6] == 1:
-            keylist.append('spacebar')
+                duration = 0.05  # actions taken every 'duration' in sec.
+                # to be set to 0 because the same action is to be taken until the next line updates.
+                t = time.time()
+                keylist = []
+                autoclicker = False
+                autoclickerstatus = False  # saves past status of autoclicker
+                mouse = tuple()
+                # ----
+                line = list(map(int, action))
+                if len(line) < 13:
+                    raise AssertionError
+                if line[0] == 1:
+                    keylist.append('w')
+                if line[1] == 1:
+                    keylist.append('a')
+                if line[2] == 1:
+                    keylist.append('s')
+                if line[3] == 1:
+                    keylist.append('d')
+                if line[4] == 1:
+                    keylist.append('shift')
+                if line[5] == 1:
+                    keylist.append('ctrl')
+                if line[6] == 1:
+                    keylist.append('spacebar')
 
-        autoclicker = bool(line[7])
-        mouse = (line[8] * 100 + line[9] * 10 + line[10], line[11] * 100 + line[12] * 10 + line[13])
-        for key in list(set(['w', 'a', 's', 'd', 'shift', 'ctrl', 'spacebar']) - set(keylist)):
-            # clear any pressed keys
-            keyboard.release(key)
-            print(key + " was released")
-        for key in (keylist):
-            # press the key according to the string.
-            keyboard.press(key)
-            print(key + ' was pressed')
-        for _ in range(500000000):
-            if time.time() - t >= duration:
-                break
+                autoclicker = bool(line[7])
+                mouse = (line[8] * 100 + line[9] * 10 + line[10], line[11] * 100 + line[12] * 10 + line[13])
+                for key in list(set(['w', 'a', 's', 'd', 'shift', 'ctrl', 'spacebar']) - set(keylist)):
+                    # clear any pressed keys
+                    keyboard.release(key)
+                    print(key + " was released")
+                for key in (keylist):
+                    # press the key according to the string.
+                    keyboard.press(key)
+                    print(key + ' was pressed')
+                for _ in range(500000000):
+                    if time.time() - t >= duration:
+                        break
 
-        t += duration  # update t
-        # ----
-        if autoclicker != autoclickerstatus:
-            # when the program needs to toggle
-            keyboard.send(acc)
-        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(((mouse[0] - 500) / 30) / 1920 * 65535.0),
-                             int(((mouse[1] - 500) / 30) / 1080 * 65535.0))  # replace 1920,1080 with screen res.
-        print(f'mouse moved by {mouse}')
-        # -------
-        autoclickerstatus = autoclicker  # save the last autoclicker value
-        print(time.time() - st)
+                t += duration  # update t
+                # ----
+                if autoclicker != autoclickerstatus:
+                    # when the program needs to toggle
+                    keyboard.send(acc)
+                win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(((mouse[0] - 500) / 30) / 1920 * 65535.0),
+                                     int(((mouse[1] - 500) / 30) / 1080 * 65535.0))  # replace 1920,1080 with screen res.
+                print(f'mouse moved by {mouse}')
+                # -------
+                autoclickerstatus = autoclicker  # save the last autoclicker value
+                print(time.time() - st)
 
 
 # STOP AFTER VICTORY/LOSS
 def force_stop():
     with open('state.txt', 'r') as text:
         state = text.read()
+
+    # 0 = Waiting; 1 = Running; 2 = Finished
     if state == '0':
         return 0
     if state == '1':
         return 1
+    if state == '2':
+        return 2
 
-def state_detection():
+def state_detection(queue1):
     while True:
-
-        while not path.isfile('img.jpg'):
-            continue
-
-        try:
-            image = Image.open(open(r"img.jpg", 'rb'))
-            pixelMap = image.load()
-        except Exception as ec:
-            image = None
+        image = queue1.get()
         if image is not None:
+            pixelMap = np.array(image)
             # WHEN "GO" APPEARS
-            pixels = [pixelMap[870, 430],pixelMap[890, 430],pixelMap[920, 430],pixelMap[950, 430],pixelMap[980, 430],pixelMap[1000, 430],pixelMap[1040, 430],pixelMap[1065, 430]]
+            pixels = [pixelMap[430, 870][:-1],pixelMap[430, 890][:-1],pixelMap[430, 920][:-1],pixelMap[430, 920][:-1],pixelMap[430, 980][:-1],pixelMap[430, 1000][:-1],pixelMap[430, 1040][:-1],pixelMap[430, 1065][:-1]]
             tv_GO = [(255,255,255),(63,63,63),(255,255,255),(62,63,65),(255,255,255),(63,63,63),(255,255,255),(63,63,63)]#:true value:
             correctList=compareTuples(pixels, tv_GO, 5)
             if len(correctList)>=7:
@@ -178,24 +157,10 @@ def state_detection():
 
 
 
-def repeat_sc():
-    while True:
-        screen_capture()
-def repeat_classify():
-    while force_stop() == 0:
-        continue
-    while force_stop() == 1:
-        classify()
-def repeat_macro():
-    while force_stop() == 0:
-        continue
-    while force_stop() == 1:
-        mojimo()
-
-
 # LAUNCH ALL PROGRAMS TOGETHER
 
 if __name__ == '__main__':
+    np.set_printoptions(threshold=np.inf)
     with open('state.txt', 'w') as text:
         text.write('0')
     user32 = windll.user32
@@ -208,8 +173,13 @@ if __name__ == '__main__':
     except FileNotFoundError:
         pass
         print('file not found')
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        executor.submit(repeat_sc)
-        executor.submit(repeat_classify)
-        executor.submit(repeat_macro)
-        executor.submit(state_detection)
+
+    # gives image
+    queue1 = Queue()
+    # gives action
+    queue2 = Queue()
+
+    Process(target=state_detection, args=(queue1,)).start()
+    Process(target=screen_grab, args=(queue1,)).start()
+    Process(target=classify, args=(queue1,queue2)).start()
+    Process(target=execute_action, args=(queue2,)).start()
